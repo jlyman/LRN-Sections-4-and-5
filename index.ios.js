b97f5a8aa86c7963c449919c9ff8fe2334cae5ad
Joshua Lyman <jlyman.work@gmail.com>
2016-08-05 11:42:16 -0500

    Redux glue in our index files

--- index.ios.js
+++ index.ios.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react'
 import {
 	AppRegistry,
 } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import App from './components/App'
+import chatNowReducers from './reducers'
+
+let store = createStore(chatNowReducers)
 
 
 class ChatNow extends Component {
 	render() {
 		return (
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
+
 		)
 	}
 }
